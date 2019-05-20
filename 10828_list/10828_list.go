aa6bac2adc94b6ecbe71a308452b278d51412024
hahooh <hahooh@gmail.com>
2019-05-20 23:24:26 +0900

    std out using bufio

--- 10828_list/10828_list.go
+++ 10828_list/10828_list.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"container/list"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,7 +13,9 @@ func main() {
 	var numCmd int
 	var cmd string
 	var el int
-	var results []int
+	buff := bufio.NewWriter(os.Stdout)
+	defer buff.Flush()
+
 	fmt.Scanf("%d", &numCmd)
 	for i := 0; i < numCmd; i++ {
 		fmt.Scanf("%s", &cmd)
@@ -25,14 +30,15 @@ func main() {
 				result = lastEl.Value.(int)
 				stack.Remove(lastEl)
 			}
-			results = append(results, result)
+			buff.WriteString(strconv.Itoa(result) + "\n")
 		case "size":
-			results = append(results, stack.Len())
+			buff.WriteString(strconv.Itoa(stack.Len()) + "\n")
 		case "empty":
-			if stack.Len() == 0 {
-				results = append(results, 1)
-			} else {
-				results = append(results, 0)
+			switch stack.Len() {
+			case 0:
+				buff.WriteString(strconv.Itoa(1) + "\n")
+			default:
+				buff.WriteString(strconv.Itoa(0) + "\n")
 			}
 		case "top":
 			lastEl := stack.Back()
@@ -40,11 +46,7 @@ func main() {
 			if lastEl != nil {
 				result = lastEl.Value.(int)
 			}
-			results = append(results, result)
+			buff.WriteString(strconv.Itoa(result) + "\n")
 		}
-	}
-
-	for _, result := range results {
-		fmt.Println(result)
 	}
 }
